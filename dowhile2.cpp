3902816fcde6dde78688d255b8d706c5c5f62166
K24-Code <kashishvijay2405@gmail.com>
2025-03-04 00:33:31 +0530

    Add files via upload

---
+++ dowhile2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+int i=6;
+do {
+    cout<< i <<" ";
+    i--;
+    }
+    while(i>=0);
+    return 0;
+}
---
+++ dowhile3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int count=20;
+    int i=1;
+    do{
+        cout<<"Kashish"<<" ";
+        i++;
+    }
+    while(i<=count);
+    return 0;
+}
